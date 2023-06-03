687a6f4fad558524a2320a75a1de4c6d242cca04
Stepan Ustimenko <50939552+Apochromat@users.noreply.github.com>
2023-06-03 11:36:55 +0700

    feat(auth, backend, files, notifications) : добавил Serilog

--- ChatNet.FileStorage.API/Program.cs
+++ ChatNet.FileStorage.API/Program.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Serilog
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
 
 var app = builder.Build();
 
